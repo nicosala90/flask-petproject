4cd3ca74b4d8792131f074366738c339b86465e7
nicosala90 <nicosala90@gmail.com>
2023-08-07 17:47:42 +0200

    add signup form

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from forms import SignUpForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "password"
 
 @app.route("/")
 def hello_world():
@@ -15,6 +17,16 @@ def home():
         "age": 80}
         ]
     return render_template('users.html', obj=obj)
+
+@app.route("/users/<user_id>")
+def user_id(user_id):
+    return "This user id is: " + user_id
+
+@app.route("/signup")
+def signup():
+    form = SignUpForm()
+    return render_template('signup.html', form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
