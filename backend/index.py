73ad2828da34c63713fdf37792b63205b9eba5fe
nicosala90 <nicosala90@gmail.com>
2023-08-15 11:42:08 +0200

    connect to database, sending data to mongodb

--- backend/index.py
+++ backend/index.py
@@ -1,16 +1,17 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
-from flask_pymongo import PyMongo
+#from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017, username='username', password='password')
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/todos_db'
-mongo = PyMongo(app)
-db = client.todos
+client = MongoClient('localhost', 27017)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017'
+#mongo = PyMongo(app)
+db = client.todos_db
+collection = db.todos
 
 @app.get('/api/all-todos')
-def get_all-todos():
+def get_all_todos():
     data = db['todo_manager'].find()
     data_list = list(data)
     return jsonify(data_list)
@@ -20,7 +21,16 @@ def post_todo():
         todo = request.form['todo']
         degree = request.form['degree']
         db.insert_one({'todo': todo, 'degree': degree})
-        
+
+@app.route('/add_data', methods=['POST'])
+def add_data():
+    # Get data from request
+    data = request.json
+  
+    # Insert data into MongoDB
+    collection.insert_one(data)
+  
+    return 'Data added to MongoDB'
 
 @app.route("/")
 def hello_world():
